35d65bc254d00b77721f60c2c3a10a0db654fe41
李想 <freewm@163.com>
2020-04-20 15:58:29 +0800

    发布

--- publish.sh
+++ publish.sh
@@ -3,6 +3,13 @@ cd src
 
 gitbook build
 
+while ["&?" != 0] 
+
+do
+
+   gitbook build
+done
+
 cp -r _book/* ../
 
 cd ../
@@ -12,4 +19,5 @@ git add .
 git commit -m "发布"
 
 git push origin master
+
 
